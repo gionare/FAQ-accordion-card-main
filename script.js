3e51144d19f7f20bbba4dec1f0d18b61f2a79e8b
gionare <gionare93@gmail.com>
2023-12-22 21:23:05 -0500

    finished mobile version

--- script.js
+++ script.js
@@ -3,38 +3,41 @@
 // to toggle the visibility of the answer panel when the
 // button is clicked.
 
-// let acc = document.getElementsByClassName("accordion");
-// console.log(acc);
-
-// for (let i = 0; i < acc.length; i++) {
-//     acc[i].addEventListener("click", function() {
-//       this.classList.toggle("active");
-//       let panel = this.nextElementSibling;
-//       if (panel.style.maxHeight) {
-//         panel.style.maxHeight = null;
-//       } else {
-//         panel.style.maxHeight = panel.scrollHeight + "px";
-//       }
-//     });
-//   }
-
-
 // using  classList.toggle
 
 // Get the button and answer panel elements
-let acc = document.querySelector(".accordion")
+let acc = document.querySelectorAll(".accordion");
 console.log(acc);
-let answerPanel = document.querySelector(".answer-panel");
-console.log(answerPanel);
+// .button #accordionContent 
+let accordionContent = document.getElementById('accordionContent');
+console.log(accordionContent);
+let arrow = document.getElementsByClassName('arrow')
+console.log(arrow);
 
-acc.addEventListener("click", function () {
-  // Toggle the 'active' class on the button to change its appearance
-  acc.classList.toggle("active");
+acc.forEach(function (accordion) {
+  let answerPanel = accordion.nextElementSibling;
+  console.log(answerPanel);
+  
+  accordion.addEventListener("click", function () {
 
-  // Toggle the visibility of the answer panel
-  if (answerPanel.style.display === "block") {
-    answerPanel.style.display = "none";
-  } else {
-    answerPanel.style.display = "block";
-  }
+    // Toggle the 'active' class on the button to change its appearance
+    // accordion.classList.toggle("active");
+    // Toggle the visibility of the answer panel
+    // Toggle the 'pressed' class on the clicked accordion
+    // accordion.classList.toggle("pressed");
+
+    if (answerPanel.style.display === "block") {
+      answerPanel.style.display = "none";
+      accordionContent.style.fontWeight = '400';
+      arrow.style.url = "images/icon-arrow-down.svg"
+    } else {
+      answerPanel.style.display = "block";
+      accordionContent.style.fontWeight = '700';
+      arrow.src = "images/icon-arrow-up.svg"
+    }
+    
+  });
 });
+
+
+//to ask --?-- need to put accordion.addEventListener in loop frobably? and choose it by loop index?!
